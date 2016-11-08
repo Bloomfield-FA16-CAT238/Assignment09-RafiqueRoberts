f302671110dcd1fa2e4e7419751a18d18cf919b1
Rafique Roberts <rafique_roberts@bloomfield.edu>
2016-11-08 16:18:15 -0500

    Finished Assignment 09

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour
+{
+
+    public GameController gameController;
+    public int damage;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameController.Damage(damage);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject respawnPoint;
+
+    public float fullHealth;
+    public Slider healthSlider;
+    float currentHealth;
+
+    // Use this for initialization
+    void Start () {
+
+        currentHealth = fullHealth;
+        healthSlider.maxValue = fullHealth;
+        healthSlider.value = fullHealth;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RespawnPlayer()
+    {
+        player.gameObject.transform.position = respawnPoint.transform.position;
+        currentHealth = fullHealth;
+        healthSlider.maxValue = fullHealth;
+        healthSlider.value = fullHealth;
+    }
+
+    public void Damage(float damage)
+    {
+        if (damage <= 0)
+            return;
+        currentHealth -= damage;
+        healthSlider.value = currentHealth;
+
+
+        if (currentHealth <= 0)
+            RespawnPlayer();
+    }
+}
